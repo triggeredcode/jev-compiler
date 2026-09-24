d42021ee0bd99248ebb2f7b969244d76e13984e0
Omkar Tripathi <omkar.tripathi101@gmail.com>
2026-09-24 21:53:38 +0530

    feat(freeze): build and verify immutable program artifacts

---
+++ src/jevcompiler/freeze/__init__.py
@@ -0,0 +1,4 @@
+from jevcompiler.freeze.builder import ArtifactError, freeze_optimization, verify_artifact
+from jevcompiler.freeze.models import FrozenManifest
+
+__all__ = ["ArtifactError", "FrozenManifest", "freeze_optimization", "verify_artifact"]
